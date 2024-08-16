aa0d5c6778f330728cc8efc95490f294e9b9b8de
LindaTa <lindata@maneandmoon.com>
2024-08-16 10:39:23 -0500

    practice completed

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 7, 'M', 'brown', 'calm', TRUE),
+('Rowdy', 10, 'M', 'black', 'aggressive', TRUE),
+('Tabitha', 6, 'F', 'brown', 'friendly', TRUE),
+('Sergeant Brown', 12, 'M', 'brown', 'strict', FALSE),
+('Melissa', 5, 'F', 'black', 'playful', TRUE),
+('Grinch', 8, 'M', 'brown', 'grumpy', TRUE),
+('Wendy', 4, 'F', 'blonde', 'shy', TRUE),
+(NULL, 3, 'M', 'gray', 'unknown', TRUE);
